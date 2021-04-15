31eda61285e905dc0311df66a0a37e742b7bc154
hoagnhat <hahng.nhat@gmail.com>
2021-04-15 15:31:55 +0700

    complete security

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
+const cookieParser = require('cookie-parser')
+var cookieSession = require('cookie-session')
 
 // Import routers
 const authRouter = require('./routes/auth.route')
+const accountRouter = require('./routes/account.route')
 
-// Environment variables
-const PORT = process.env.PORT
+// Import middlewares
+const authMiddleware = require('./middlewares/auth.middleware')
+
+// Session config
+app.use(cookieParser(process.env.SECRET))
+app.use(cookieSession({
+    name: 'session',
+    secret: process.env.SECRET,
+    maxAge: 3 * 60 * 60 * 1000 // 3 hours
+}))
 
 // Config mongoose to connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URL , {useNewUrlParser: true, useUnifiedTopology: true})
@@ -28,5 +39,6 @@ app.use(express.urlencoded({ extended: true }))
 
 // Routes
 app.use('/', authRouter)
+app.use('/accounts', authMiddleware.requireAuth, accountRouter)
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,8 +5,11 @@ const controller = require('../controller/auth.controller')
 
 // GET Requests
 router.get('/login', controller.GetLogin)
+router.get('/register', controller.GetRegister)
+router.get('/logout', controller.GetLogout)
 
 // POST Requests
 router.post('/login', controller.PostLogin)
+router.post('/register', controller.PostRegister)
 
 module.exports = router
